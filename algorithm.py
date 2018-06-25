aac01222c24eb0a852db2f2fcb18d3f7be2f3e37
Samuel Müller <samuel.mueller@bigriverintl.com>
2018-06-25 22:04:08 +0200

    support for multiple layers in parallel added

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,6 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
-from multiprocessing import Pool
 import tensorflow as tf
 
 from tensorflow.examples.tutorials.mnist import input_data
@@ -29,8 +28,9 @@ parser.add_argument("--samplesupremum", type=int, default=5)
 parser.add_argument('--trainingsteps', type=int, default=18000)
 parser.add_argument('--firstlayersize', type=int, default=300)
 parser.add_argument('--secondlayersize', type=int, default=100)
+parser.add_argument('--layerrepetitions', type=str, default='[1,1]')
 parser.add_argument('--remainingcounts', type=str,
-                    default='[(round(share*784*300), round(share*300*100), round(share*100*10)) ' +
+                    default='[((round(share*784*300),), (round(share*300*100),), (round(share*100*10),)) ' +
                     'for share in ((.8**2)**i for i in range(1, 11))]')
 args = parser.parse_args()
 assert(args.resultdir is not None)
@@ -43,6 +43,7 @@ SAMPLE_SUPREMUM = args.samplesupremum
 FIRST_LAYER_SIZE = args.firstlayersize
 SECOND_LAYER_SIZE = args.secondlayersize
 REMAINING_COUNTS = eval(args.remainingcounts)
+LAYER_REPETITIONS = eval(args.layerrepetitions)
 
 print('Results written to {}'.format(RESULT_PATH_PREFIX))
 print('Iterative Run {}'.format(ITERATIVE))
@@ -65,26 +66,57 @@ x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
 prune = []
-prune.append(tf.placeholder(tf.float32, shape=[784, FIRST_LAYER_SIZE]))
-prune.append(tf.placeholder(tf.float32, shape=[
-             FIRST_LAYER_SIZE, SECOND_LAYER_SIZE]))
-prune.append(tf.placeholder(tf.float32, shape=[SECOND_LAYER_SIZE, 10]))
+prune.append(
+    [tf.placeholder(tf.float32, shape=[784, FIRST_LAYER_SIZE])] *
+    LAYER_REPETITIONS[0]
+)
+prune.append(
+    [tf.placeholder(tf.float32, 
+                    shape=[FIRST_LAYER_SIZE,
+                           SECOND_LAYER_SIZE])]*LAYER_REPETITIONS[1]
+)
+prune.append(
+    [tf.placeholder(tf.float32, shape=[SECOND_LAYER_SIZE, 10])]
+)
+
+first_layers_per_second_layer = LAYER_REPETITIONS[0] // LAYER_REPETITIONS[1]
 
 W = []
 b = []
 h_layer = []
 
-W.append(weight_variable([784, FIRST_LAYER_SIZE]))
-b.append(bias_variable([FIRST_LAYER_SIZE]))
-h_layer.append(tf.nn.relu(tf.matmul(x, W[0] * prune[0]) + b[0]))
+W.append(
+    [weight_variable([784, FIRST_LAYER_SIZE])]*LAYER_REPETITIONS[0]
+)
+b.append(
+    [bias_variable([FIRST_LAYER_SIZE])] * LAYER_REPETITIONS[0]
+)
+h_layer.append(
+    [tf.nn.relu(tf.matmul(x, W[0][i] * prune[0][i]) + b[0][i])
+     for i in range(LAYER_REPETITIONS[0])]
+)
 
-W.append(weight_variable([FIRST_LAYER_SIZE, SECOND_LAYER_SIZE]))
-b.append(bias_variable([SECOND_LAYER_SIZE]))
-h_layer.append(tf.nn.relu(tf.matmul(h_layer[0], W[1] * prune[1]) + b[1]))
+W.append(
+    [weight_variable([FIRST_LAYER_SIZE, SECOND_LAYER_SIZE])] *
+    LAYER_REPETITIONS[1]
+)
+b.append(
+    [bias_variable([SECOND_LAYER_SIZE])] * LAYER_REPETITIONS[1]
+)
+next_h_layer = []
+for i in range(LAYER_REPETITIONS[1]):
+    all_inputs = []
+    for k in range(first_layers_per_second_layer):
+        j = i * first_layers_per_second_layer + k
+        all_inputs.append(h_layer[0][j])
+    sum_input = tf.reduce_sum(all_inputs, axis=0)
+    next_h_layer.append(tf.nn.relu(
+        tf.matmul(sum_input, W[1][i] * prune[1][i]) + b[1][i]))
+h_layer.append(next_h_layer)
 
-W.append(weight_variable([SECOND_LAYER_SIZE, 10]))
-b.append(bias_variable([10]))
-y = tf.matmul(h_layer[1], W[2] * prune[2]) + b[2]
+W.append([weight_variable([SECOND_LAYER_SIZE, 10])])
+b.append([bias_variable([10])])
+y = tf.matmul(tf.reduce_sum(h_layer[-1], axis=0), W[2][0] * prune[2][0]) + b[2][0]
 
 cross_entropy = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
@@ -102,7 +134,7 @@ def train(p):
             acc = accuracy.eval(feed_dict={
                 x: mnist.test.images,
                 y_: mnist.test.labels,
-                **{p_ph: pr for p_ph, pr in zip(prune, p)}
+                **{p_ph: pr for p_list in zip(prune, p) for p_ph, pr in zip(*p_list)}
             })
             accuracies.append(acc)
             iterations.append(i)
@@ -110,7 +142,7 @@ def train(p):
         train_step.run(feed_dict={
             x: batch[0],
             y_: batch[1],
-            **{p_ph: pr for p_ph, pr in zip(prune, p)}
+            **{p_ph: pr for p_list in zip(prune, p) for p_ph, pr in zip(*p_list)}
         })
     return accuracies, iterations
 
@@ -133,13 +165,13 @@ def evaluate_sample(sample_n):
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
 
-    W_init = [w.eval() for w in W]
+    W_init = [[w.eval() for w in layer] for layer in W]
 
-    base_accuracies, base_iterations = train(
-        [np.ones([784, FIRST_LAYER_SIZE]),
-         np.ones([FIRST_LAYER_SIZE, SECOND_LAYER_SIZE]),
-         np.ones([SECOND_LAYER_SIZE, 10])]
-    )
+    p = [[np.ones([784, FIRST_LAYER_SIZE])]*LAYER_REPETITIONS[0],
+         [np.ones([FIRST_LAYER_SIZE, SECOND_LAYER_SIZE])]*LAYER_REPETITIONS[1],
+         [np.ones([SECOND_LAYER_SIZE, 10])]]
+
+    base_accuracies, base_iterations = train(p)
 
     np.save('{}/base_accuracies_{}.npy'.format(
         RESULT_PATH_PREFIX, sample_n), base_accuracies)
@@ -148,21 +180,20 @@ def evaluate_sample(sample_n):
 
     W_after_train = [w.eval() for w in W]
 
-    p = [np.ones([784, FIRST_LAYER_SIZE]),
-         np.ones([FIRST_LAYER_SIZE, SECOND_LAYER_SIZE]),
-         np.ones([SECOND_LAYER_SIZE, 10])]
-
     for counts in REMAINING_COUNTS:
         if ITERATIVE:
             p = [create_prune(w.eval()*pr, c)
-                 for w, pr, c in zip(W, p, counts)]
+                 for layer in zip(W, p, counts)
+                 for w, pr, c in zip(*layer)]
         else:
             p = [create_prune(w_after_train, c)
-                 for w_after_train, c in zip(W_after_train, counts)]
+                 for layer in zip(W_after_train, counts)
+                 for w_after_train, c in zip(*layer)]
 
         sess.run(tf.global_variables_initializer())
-        for w, w_init in zip(W, W_init):
-            sess.run(w.assign(w_init))
+        for layer in zip(W, W_init):
+            for w, w_init in zip(*layer):
+                sess.run(w.assign(w_init))
 
         accuracies, iterations = train(p)
 
